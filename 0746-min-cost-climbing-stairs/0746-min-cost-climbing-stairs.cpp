ed4bb160f586cf860353c3fddbade44a6cec2b0b
Tanvir Mahamood <mahamoodtanvir1@gmail.com>
2026-08-15 20:35:48 +0600

    Time: 0 ms (100%), Space: 17.8 MB (10.19%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // Top Down Approach (Memoization) (Time: O(n), Space: O(n))
+    /*
     int solve(vector<int>& cost, int idx, vector<int> &dp) {
         if(idx <= 1) return cost[idx]; 
         if(dp[idx] != -1) return dp[idx];
@@ -10,5 +12,18 @@ public:
         int n = cost.size();
         vector<int> dp(n, -1);
         return solve(cost, n-1, dp);
+    }
+    */
+    // Bottom Up Approach (Tabulation) (Time: O(n), Space: O(n))
+    int minCostClimbingStairs(vector<int>& cost) {
+        cost.push_back(0);
+        int n = cost.size();
+        vector<int> dp(n);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i=2; i<n; i++) {
+            dp[i] = min(dp[i-1], dp[i-2]) + cost[i];
+        }
+        return dp.back();
     }
 };
