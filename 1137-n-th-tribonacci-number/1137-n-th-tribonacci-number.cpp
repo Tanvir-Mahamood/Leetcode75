fbef6a586d4e678208c8e79580330805af6b5880
Tanvir Mahamood <mahamoodtanvir1@gmail.com>
2026-08-15 11:54:27 +0600

    Time: 0 ms (100%), Space: 7.8 MB (69.96%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+    // Bottom Up (Tabulation) (Time: O(n), Space: O(n))
+    int tribonacci(int n) {
+        if(n == 0) return 0;
+        else if(n < 3) return 1;
+
+        int dp[n+1];
+        dp[0] = 0, dp[1] = 1, dp[2] = 1;
+        for(int i=3; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        return dp[n];
+    }
+
+    // Space Optimised Bottom Up (Time: O(n), Space: O(1))
+    /*
     int tribonacci(int n) {
         int a0 = 0, a1 = 1, a2 = 1, s;
         if(n == 0) return a0;
@@ -12,4 +27,7 @@ public:
         }
         return s;
     }
+    */
+
+    
 };
