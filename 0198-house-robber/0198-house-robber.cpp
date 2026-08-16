1dba8fc25a6b0b162f8ed3e527b9ce66f43eb453
Tanvir Mahamood <mahamoodtanvir1@gmail.com>
2026-08-16 20:28:58 +0600

    Time: 0 ms (100%), Space: 10.7 MB (24.17%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    // Bottom Up (Tabulation) (Time: O(n), Space: O(n))
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums.front();
+
+        vector<int> dp(n);
+        dp[0] = nums[0], dp[1] = max(nums[1], dp[0]);
+        for(int i=2; i<n; i++) {
+            dp[i] = max(nums[i]+dp[i-2], dp[i-1]); // max(pick, not pick)
+        }
+        return dp.back();
+    }
+};
