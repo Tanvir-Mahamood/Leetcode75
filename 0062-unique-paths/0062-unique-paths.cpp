426f87930dc1732125d68b5a1f158f220ab3774e
Tanvir Mahamood <mahamoodtanvir1@gmail.com>
2026-08-20 10:11:46 +0600

    Time: 0 ms (100%), Space: 8.3 MB (77.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     // Bottom Up (Time: O(mn), Space: O(mn))
+    /*
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n));
         for(int j=0; j<n; j++) dp[0][j] = 1;
@@ -11,5 +12,18 @@ public:
             }
         }
         return dp[m-1][n-1];
+    }
+    */
+
+    // Bottom Up (Space Optimized) (Time: O(mn), Space: O(n))
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(2,vector<int>(n, 1));
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                if(i&1) dp[1][j] = dp[0][j] + dp[1][j-1];
+                else dp[0][j] = dp[1][j] + dp[0][j-1];
+            }
+        }
+        return dp[(m&1)^1][n-1];
     }
 };
